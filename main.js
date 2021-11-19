d42219f8b92ef6994f0a6bb0695d6e313e33c6e7
rebjannik <71289800+rebjannik@users.noreply.github.com>
2021-11-19 15:44:20 +0100

    Reset button added + css fixed

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const showPicture = () => {
     document.getElementById('picture').style.display = "block";
+    document.getElementById("reset").style.display = "block";
+    document.getElementById("show").style.display = "none";
   } 
+
+  const hidePicture = () => {
+      document.getElementById('picture').style.display = "none";
+      document.getElementById("reset").style.display = "none";
+    document.getElementById("show").style.display = "block";
+  }
